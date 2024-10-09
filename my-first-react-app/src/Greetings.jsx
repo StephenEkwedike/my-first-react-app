23b46c53f7420adc5ba402d49553acc42f313cd9
StephenEkwedike <stephen.toch@gmail.com>
2024-10-08 18:59:09 -0700

    Added a new funcional component

---
+++ my-first-react-app/src/Greetings.jsx
@@ -0,0 +1,7 @@
+function Greeting(){
+    return <h1>&quot;I swear by my pretty floral bonnet, I will end you.&quot;</h1>
+}
+export function Goodbye(){
+    return <h2>See you later</h2>
+}
+export default Greeting
--- my-first-react-app/src/main.jsx
+++ my-first-react-app/src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import Greeting from './greetings.jsx'
+import { Goodbye } from './greetings.jsx'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Greeting/>
+    <Goodbye/>
   </StrictMode>,
 )
